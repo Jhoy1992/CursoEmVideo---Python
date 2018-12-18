9d1dd236a42bcc95a5321d976e20612508d4eca4
jhoyascari <jhonatanascari@gmail.com>
2018-12-18 11:09:54 -0200

    Added lesson 12.

---
+++ lesson012.py
@@ -0,0 +1,8 @@
+name = input('Type a name: ')
+
+if name == 'Jhonatan':
+    print('This is a beautiful name {}.'.format(name))    
+elif name == 'Letícia':
+    print('This is a beautiful name for a girl {}.'.format(name))
+else:
+    print('Your name is not awesome {}.'.format(name))
